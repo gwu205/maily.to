8fd9c0d711ff4b1014c9264c061838ed6e69c2cf
Geoffrey Wu <38195698+gwu205@users.noreply.github.com>
2025-12-04 18:31:09 +0900

    Remove email sending

--- apps/web/app/components/email-editor-sandbox.tsx
+++ apps/web/app/components/email-editor-sandbox.tsx
@@ -1,23 +1,13 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import type { Editor, FocusPosition } from '@tiptap/core';
-import {
-  AsteriskIcon,
-  FileCogIcon,
-  Loader2Icon,
-  SaveIcon,
-  XIcon,
-} from 'lucide-react';
+import { FileCogIcon, Loader2Icon, SaveIcon } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useNavigate, useRevalidator } from 'react-router';
 import { toast } from 'sonner';
 import { cn } from '~/lib/classname';
 import defaultEmailJSON from '~/lib/default-editor-json.json';
-import { FetchError, httpPost } from '~/lib/http';
+import { httpPost } from '~/lib/http';
 import type { Database } from '~/types/database';
-import {
-  ApiKeyConfigDialog,
-  apiKeyQueryOptions,
-} from './api-key-config-dialog';
 import { CopyEmailHtml } from './copy-email-html';
 import { DeleteEmailDialog } from './delete-email-dialog';
 import { EmailEditor } from './email-editor';
@@ -55,7 +45,6 @@ export function EmailEditorSandbox(props: EmailEditorSandboxProps) {
 
   const navigate = useNavigate();
   const revalidator = useRevalidator();
-  const { data: apiKeyConfig } = useQuery(apiKeyQueryOptions());
   const [generatedHtml, setGeneratedHtml] = useState('');
 
   const [currentLanguage, setCurrentLanguage] = useState<Language>('en');
@@ -267,25 +256,6 @@ export function EmailEditorSandbox(props: EmailEditorSandboxProps) {
       },
     });
 
-  const { mutateAsync: sendTestEmail, isPending: isSendTestEmailPending } =
-    useMutation({
-      mutationFn: async () => {
-        const json = editor?.getJSON();
-        if (!json) {
-          throw new FetchError(400, 'Editor content is empty');
-        }
-
-        return httpPost(`/api/v1/emails/send`, {
-          subject,
-          previewText,
-          from,
-          to,
-          replyTo,
-          content: JSON.stringify(json),
-        });
-      },
-    });
-
   return (
     <div className="flex h-[calc(100vh-4rem)]">
       <div className="w-1/2 overflow-y-auto p-4">
@@ -295,35 +265,12 @@ export function EmailEditorSandbox(props: EmailEditorSandboxProps) {
             onLanguageChange={handleLanguageChange}
           />
           <div className="flex items-center gap-1.5">
-            <ApiKeyConfigDialog
-              apiKey={apiKeyConfig?.apiKey}
-              provider={apiKeyConfig?.provider}
-            />
             <PreviewEmailDialog
               subject={subject}
               previewText={previewText}
               editor={editor}
             />
             <CopyEmailHtml previewText={previewText} editor={editor} />
-            <button
-              className="flex items-center rounded-md bg-white px-2 py-1 text-sm text-black hover:bg-gray-100 disabled:cursor-not-allowed disabled:opacity-50"
-              type="submit"
-              disabled={isSendTestEmailPending}
-              onClick={() => {
-                toast.promise(sendTestEmail(), {
-                  loading: 'Sending Test Email...',
-                  success: 'Test Email has been sent',
-                  error: (err) => err?.message || 'Failed to send test email',
-                });
-              }}
-            >
-              {isSendTestEmailPending ? (
-                <Loader2Icon className="mr-1 inline-block size-4 animate-spin" />
-              ) : (
-                <AsteriskIcon className="mr-1 inline-block size-4" />
-              )}
-              Send Email
-            </button>
           </div>
 
           {!template?.id && showSaveButton && (
@@ -431,70 +378,6 @@ export function EmailEditorSandbox(props: EmailEditorSandboxProps) {
               type="text"
               value={subject}
               onChange={(event) => setSubject(event.target.value)}
-            />
-          </Label>
-          <div className="flex items-center gap-1.5">
-            <Label className="flex grow items-center font-normal">
-              <span className="w-20 shrink-0 font-normal text-gray-600">
-                From
-              </span>
-              <Input
-                className="h-auto rounded-none border-none py-2.5 font-normal focus-visible:ring-0 focus-visible:ring-offset-0"
-                placeholder="Arik Chakma <hello@maily.to>"
-                type="text"
-                value={from}
-                onChange={(event) => setFrom(event.target.value)}
-              />
-            </Label>
-
-            {!showReplyTo && (
-              <button
-                className="inline-block h-full shrink-0 bg-transparent px-1 text-sm text-gray-500 hover:text-gray-700 disabled:cursor-not-allowed disabled:text-gray-400 disabled:opacity-50 disabled:hover:bg-transparent disabled:hover:text-gray-400"
-                type="button"
-                onClick={() => {
-                  setShowReplyTo(true);
-                }}
-              >
-                Reply-To
-              </button>
-            )}
-          </div>
-
-          {showReplyTo && (
-            <Label className="flex items-center font-normal">
-              <span className="w-20 shrink-0 font-normal text-gray-600">
-                Reply-To
-              </span>
-              <div className="align-content-stretch flex grow items-center">
-                <Input
-                  className="h-auto rounded-none border-none py-2.5 font-normal focus-visible:ring-0 focus-visible:ring-offset-0"
-                  placeholder="noreply@maily.to"
-                  type="text"
-                  value={replyTo}
-                  onChange={(event) => setReplyTo(event.target.value)}
-                />
-                <button
-                  className="flex h-10 shrink-0 items-center bg-transparent px-1 text-gray-500 hover:text-gray-700 disabled:cursor-not-allowed disabled:opacity-50"
-                  type="button"
-                  onClick={() => {
-                    setReplyTo('');
-                    setShowReplyTo(false);
-                  }}
-                >
-                  <XIcon className="inline-block size-4" />
-                </button>
-              </div>
-            </Label>
-          )}
-
-          <Label className="flex items-center font-normal">
-            <span className="w-20 shrink-0 font-normal text-gray-600">To</span>
-            <Input
-              className="h-auto rounded-none border-none py-2.5 font-normal focus-visible:ring-0 focus-visible:ring-offset-0"
-              placeholder="Email Recipient(s)"
-              type="text"
-              value={to}
-              onChange={(event) => setTo(event.target.value)}
             />
           </Label>
 
--- apps/web/app/components/email-editor.tsx
+++ apps/web/app/components/email-editor.tsx
@@ -1,6 +1,8 @@
+'use client'; // If using Next.js, otherwise remove this line
+
 import type { FocusPosition, Editor as TiptapEditor } from '@tiptap/core';
 import { Loader2Icon } from 'lucide-react';
-import { lazy, Suspense, useState } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import { cn } from '~/lib/classname';
 import type { Database } from '~/types/database';
 
@@ -12,14 +14,45 @@ const Editor = lazy(() =>
 
 type EmailEditorProps = {
   defaultContent: Database['public']['Tables']['mails']['Row']['content'];
-  setEditor: (editor: TiptapEditor) => void;
+  setEditor: (editor: TiptapEditor | null) => void;
   autofocus?: FocusPosition;
 };
 
-export function EmailEditor(props: EmailEditorProps) {
-  const { defaultContent, setEditor, autofocus } = props;
-
+export function EmailEditor({
+  defaultContent,
+  setEditor,
+  autofocus,
+}: EmailEditorProps) {
   const [isLoading, setIsLoading] = useState(true);
+  const [isMounted, setIsMounted] = useState(false);
+
+  // Set isMounted to true on component mount
+  useEffect(() => {
+    setIsMounted(true);
+    return () => {
+      // Cleanup editor reference on unmount
+      setEditor(null);
+      setIsMounted(false);
+    };
+  }, [setEditor]);
+
+  // Don't render anything on the server
+  if (!isMounted) {
+    return (
+      <div className="flex w-full items-center justify-center py-10">
+        <Loader2Icon className="h-8 w-8 animate-spin stroke-[2.5] text-gray-500" />
+      </div>
+    );
+  }
+
+  const handleEditorCreate = (editor: TiptapEditor) => {
+    setIsLoading(false);
+    setEditor(editor);
+  };
+
+  const handleEditorUpdate = (editor: TiptapEditor) => {
+    setEditor(editor);
+  };
 
   return (
     <>
@@ -29,30 +62,28 @@ export function EmailEditor(props: EmailEditorProps) {
         </div>
       )}
 
-      <Suspense>
-        <Editor
-          config={{
-            hasMenuBar: false,
-            wrapClassName: cn('editor-wrap', isLoading && 'hidden'),
-            bodyClassName: '!mt-0 !border-0 !p-0',
-            contentClassName: `editor-content mx-auto max-w-[calc(600px+80px)]! px-10! pb-10!`,
-            toolbarClassName: 'flex-wrap !items-start',
-            spellCheck: false,
-            autofocus,
-            immediatelyRender: false,
-          }}
-          contentJson={
-            defaultContent ? JSON.parse(defaultContent as string) : null
-          }
-          onCreate={(editor) => {
-            setIsLoading(false);
-            setEditor(editor);
-          }}
-          onUpdate={(editor) => {
-            setEditor(editor);
-          }}
-        />
-      </Suspense>
+      <div className={cn('w-full', isLoading && 'hidden')}>
+        <Suspense fallback={<div>Loading editor...</div>}>
+          <Editor
+            config={{
+              hasMenuBar: false,
+              wrapClassName: 'editor-wrap',
+              bodyClassName: '!mt-0 !border-0 !p-0',
+              contentClassName:
+                'editor-content mx-auto max-w-[calc(600px+80px)]! px-10! pb-10!',
+              toolbarClassName: 'flex-wrap !items-start',
+              spellCheck: false,
+              autofocus,
+              immediatelyRender: false,
+            }}
+            contentJson={
+              defaultContent ? JSON.parse(defaultContent as string) : null
+            }
+            onCreate={handleEditorCreate}
+            onUpdate={handleEditorUpdate}
+          />
+        </Suspense>
+      </div>
     </>
   );
 }
